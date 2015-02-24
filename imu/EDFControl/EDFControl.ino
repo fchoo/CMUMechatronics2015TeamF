6c9940f98edb320855698000dde0a6061c738248
Foo Lai Choo <choofoolai@gmail.com>
2015-02-24 01:16:39 -0500

    updated with debug prints

--- imu/EDFControl/EDFControl.ino
+++ imu/EDFControl/EDFControl.ino
@@ -1,6 +1,6 @@
 /**
-* This program controls the EDF fan speed based on the angle of inclination the 
-* robot is experiencing. First, it calculates the suction force required to keep 
+* This program controls the EDF fan speed based on the angle of inclination the
+* robot is experiencing. First, it calculates the suction force required to keep
 * the robot adhere to an inclined surface. It then adjust the EDF motor accordingly.
 **/
 
@@ -8,22 +8,22 @@
 #define ToRad(x) ((x)*0.01745329252)  // *pi/180
 #define ToDeg(x) ((x)*57.2957795131)  // *180/pi
 // Suction Force Calculation
-#define COE_FRICTION = 0.6
-#define F_WEIGHT = 3 // robot weight = 3kg
+#define COE_FRICTION 0.6
+#define F_WEIGHT (3*9.81) // robot weight 3kg
 // EDF Control
-#define PWM_MIN = 191;
-#define PWM_MAX = 255;
-#define PWM_DELAY = 20; // 20ms
-#define PWM_STEPSIZE = 5;
+#define PWM_MIN 191
+#define PWM_MAX 255
+#define PWM_DELAY 20 // 20ms
+#define PWM_STEPSIZE 5
 // Pin Definition
 #define PIN_LED 13
-#define PIN_EDF = 3; // Digital PWM pin for EDF.
+#define PIN_EDF 3 // Digital PWM pin for EDF.
 
-float pitch;
+float theta;
 float fs;
 float fs_old;
 int pwm_value = PWM_MIN;
-long timer = 0; // timer for PWM stepping
+long pwm_timer = 0; // pwm_timer for PWM stepping
 
 void setup()
 {
@@ -32,11 +32,11 @@ void setup()
   pinMode(PIN_EDF, OUTPUT);
   digitalWrite(PIN_LED, LOW); // OFF - Initializing
 
-  AHRS_Init();
   analogWrite(PIN_EDF, 0);
   delay(500);
   analogWrite(PIN_EDF, PWM_MIN);
   delay(4000);
+  AHRS_Init();
 
   digitalWrite(PIN_LED, HIGH); // ON - Initialization done
 }
@@ -47,18 +47,17 @@ void loop() //Main Loop
   calc_fs();
   if (fs > fs_old)
     step_PWM();
-  Serial.print(fs);
 }
 
 void step_PWM()
 {
-  if((millis()-timer)>=PWM_DELAY)
+  if((millis()-pwm_timer)>=PWM_DELAY)
   {
     if (pwm_value < PWM_MAX)
     {
       pwm_value += PWM_STEPSIZE;
       analogWrite(PIN_EDF, pwm_value);
-      timer = millis();
+      pwm_timer = millis();
       fs_old = fs; // Update fs
     }
   }
@@ -66,6 +65,14 @@ void step_PWM()
 
 void calc_fs()
 {
-  pitch = ToRad(Get_Pitch());
-  fs = ((F_WEIGHT*sin(pitch)) - (COE_FRICTION*F_WEIGHT*cos(pitch)))/COE_FRICTION;
+  theta = ToRad(Get_Pitch());
+  fs = ((F_WEIGHT*sin(theta)) - (COE_FRICTION*F_WEIGHT*cos(theta)))/COE_FRICTION;
+  Serial.print("Pitch: ");
+  Serial.print(Get_Pitch());
+  Serial.print("|");
+  Serial.print(theta);
+  Serial.print("   fs: ");
+  Serial.print(fs);
+  Serial.print("|");
+  Serial.println(fs/9.81);
 }
